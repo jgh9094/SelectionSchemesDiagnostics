2a9d51c0f857ac7705413346e817f30cb961d0e9
JoseHernandez9094 <josexavi06@gmail.com>
2019-09-23 14:54:24 -0400

    added fucntions to calculate scores internally and keep track of best performance

--- Experiments/source/DiagnosticOrg.h
+++ Experiments/source/DiagnosticOrg.h
@@ -8,7 +8,7 @@
 #include "base/vector.h"
 
 ///< Standard Headers
-
+#include <cmath>
 
 ///< Custom Type Names
 using genome_t = emp::vector<double>; 
@@ -16,13 +16,19 @@ using genome_t = emp::vector<double>;
 class DiaOrg {
 
   private:
-    genome_t genome;    ///< Contains the K internal values    
+    genome_t genome;          ///< Contains the K internal values    
+    genome_t score;           ///< Contains the scores per K internal values
+    double best_score;        ///< Contains the best score 
+    size_t best_index;        ///< Contains the index for the best score
+    size_t K;                 ///< How many internal values do you have
+    double total;             ///< Contains the total error
 
   public:
 
     /* Constructor, Destructors, and Copy Constructors */
-    DiaOrg(size_t K) {
+    DiaOrg(size_t K_) : best_index(-1), best_score(100000), K(K_), total(0.0) {
       genome.resize(K, 0.0);
+      score.resize(K, -1.0);
     }
     
     DiaOrg(const DiaOrg &) = default;
@@ -37,6 +43,45 @@ class DiaOrg {
 
     /* Setters */
     void SetGenome(genome_t g) { genome = g; }
+
+    /* Calulating scores */
+
+    void CalculateScore(size_t & i, double & truth);        ///< Calculate the score for a internal value
+    double TotalSumScores();                                 ///< Calculate the total error
 };
+
+void DiaOrg::CalculateScore(size_t & i, double & truth) {
+  // Make sure we are accessing a valid index
+  emp_assert(i > = 0, i);
+  emp_assert(i < K, i);
+
+  // Calculate the score internally 
+  double diff = std::abs(truth - genome[i]);
+  score[i] = diff;
+
+  // Keep track of the best score and index 
+  if(diff < best_score) {
+    best_score = diff;
+    best_index = i;
+  }
+}
+
+double DiaOrg::TotalSumScores() {
+  // Make sure they are the same size and total is new!
+  emp_assert(score.size() == K, score.size());
+  emp_assert(total == 0.0, total);
+
+  // Sum up the errors!
+  double 
+  for(size_t i = 0; i < K; ++i) {
+    // Make sure we have evaluated this internal value
+    emp_assert(score[i] != -1.0, score[i]);
+
+    // Sum up
+    total += score[i];
+  }
+
+  return total; 
+}
 
 #endif
