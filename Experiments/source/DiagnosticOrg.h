a551f9024d3599ec0fc84cf6531785d1d05774be
JoseHernandez9094 <josexavi06@gmail.com>
2019-09-25 14:02:45 -0400

    added code to reset the org stats

--- Experiments/source/DiagnosticOrg.h
+++ Experiments/source/DiagnosticOrg.h
@@ -9,6 +9,7 @@
 
 ///< Standard Headers
 #include <cmath>
+#include <limits.h>
 
 ///< Custom Type Names
 using genome_t = emp::vector<double>; 
@@ -26,12 +27,13 @@ class DiaOrg {
   public:
 
     /* Constructor, Destructors, and Copy Constructors */
-    DiaOrg(size_t K_) : best_index(-1), best_score(100000), K(K_), total(0.0) {
+    DiaOrg(size_t K_) : best_index(UINT_MAX), best_score(100000), K(K_), total(-1.0) {
       genome.resize(K, 0.0);
       score.resize(K, -1.0);
     }
     
     DiaOrg(const DiaOrg &) = default;
+    DiaOrg(const genome_t g_) : best_index(UINT_MAX), best_score(100000), total(-1.0) { genome = g_; } 
     DiaOrg(DiaOrg &&) = default;
     ~DiaOrg() { ; }
     DiaOrg & operator=(const DiaOrg &) = default;
@@ -40,19 +42,25 @@ class DiaOrg {
     /* Getters */ 
     const genome_t & GetConstGenome() const { return genome; }
     genome_t & GetGenome() { return genome; }
+    double GetTotal() const { emp_assert(total != -1.0); return total; }
 
     /* Setters */
     void SetGenome(genome_t g) { genome = g; }
+    void Reset(size_t K_); 
 
     /* Calulating scores */
 
     void CalculateScore(size_t & i, double & truth);        ///< Calculate the score for a internal value
     double TotalSumScores();                                 ///< Calculate the total error
+
+    /* DEBUGGING */
+
+    void PrintStats(); 
 };
 
 void DiaOrg::CalculateScore(size_t & i, double & truth) {
   // Make sure we are accessing a valid index
-  emp_assert(i > = 0, i);
+  emp_assert(i >= 0, i);
   emp_assert(i < K, i);
 
   // Calculate the score internally 
@@ -69,7 +77,9 @@ void DiaOrg::CalculateScore(size_t & i, double & truth) {
 double DiaOrg::TotalSumScores() {
   // Make sure they are the same size and total is new!
   emp_assert(score.size() == K, score.size());
-  emp_assert(total == 0.0, total);
+  emp_assert(total == -1.0, total);
+
+  total = 0.0;
 
   // Sum up the errors!
   for(size_t i = 0; i < K; ++i) {
@@ -83,4 +93,26 @@ double DiaOrg::TotalSumScores() {
   return total; 
 }
 
+void DiaOrg::PrintStats() {
+  // Print out all the orgs stuff
+  std::cerr << "Genome={";
+  for(auto & i : genome) { std::cerr << i << ",";}
+  std::cerr << "}" << std::endl;
+
+  std::cerr << "Score={";
+  for(auto & i : score) { std::cerr << i << ",";}
+  std::cerr << "}" << std::endl;
+
+  std::cerr << "Total=" << total << std::endl;
+  std::cerr << "best_score=" << best_score << std::endl;
+  std::cerr << "best_index=" << best_index << std::endl;
+}
+
+void DiaOrg::Reset(size_t K_) { 
+  K = K_; 
+  score.resize(K, -1.0); 
+  total = -1.0; 
+  best_index = UINT_MAX;
+  best_score = 100000;
+}
 #endif
