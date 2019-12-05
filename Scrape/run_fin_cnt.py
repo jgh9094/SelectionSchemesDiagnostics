95af0721b119365ced18b8ec3fa29c46e1c6df38
JoseHernandez9094 <josexavi06@gmail.com>
2019-12-05 17:09:54 -0500

    chaning for hpcc

--- Scrape/run_fin_cnt.py
+++ Scrape/run_fin_cnt.py
@@ -22,14 +22,14 @@ REPLICATES = 100
 NOW = datetime.datetime.now()
 
 
-############ LEXICASE STUFF ############
+######################## LEXICASE STUFF ########################
 LEX_POP_SIZE = [10, 100, 500, 700, 1000]
 LEX_OFFSET = 0
 LEX_DIR_1 = "SEL_LEXICASE__DIA_Exploitation__POP_"
 LEX_DIR_2 = "__TRT_100__SEED_"
 
 def lex(d_dir):
-  print('-----------------------------')
+  print('-----------------------------'*4)
   print('Processing Lexicase Runs-' + str(NOW))
 
   lost = []
@@ -37,7 +37,7 @@ def lex(d_dir):
   for i in range(len(LEX_POP_SIZE)):
     for r in range(1,REPLICATES+1):
       # Create the directory
-      seed = (r + (i * 100)) + LEX_OFFSET
+      seed = (r + (i * 100)) + LEX_OFFSET + REPLICATION_OFFSET
       dir = d_dir + LEX_DIR_1 + str(LEX_POP_SIZE[i]) + LEX_DIR_2 + str(seed)
 
       # Check to see if directory exists
@@ -59,19 +59,70 @@ def lex(d_dir):
 
 
   print('SEED INCOMPLETE/UNCREATED=', lost)
+  print('-----------------------------'*4)
+  print()
 
+
+
+######################## TOURNAMENT STUFF ########################
+TRN_SIZE = [7, 100, 500, 700, 1000]
+TRN_OFFSET = 500
+TRN_DIR_1 = "SEL_TOURNAMENT__DIA_Exploitation__POP___TRT_100__TOURN_"
+TRN_DIR_2 = "__SEED_"
+
+def tour(d_dir):
+  print('-----------------------------'*4)
+  print('Processing Tournament Runs-' + str(NOW))
+
+  lost = []
+
+  for i in range(len(TRN_SIZE)):
+    for r in range(1,REPLICATES+1):
+      # Create the directory
+      seed = (r + (i * 100)) + TRN_OFFSET + REPLICATION_OFFSET
+      dir = d_dir + TRN_DIR_1 + str(TRN_SIZE[i]) + TRN_DIR_2 + str(seed)
+
+      # Check to see if directory exists
+      if(os.path.isdir(dir)):
+        # Get the last row and check if we finished the run
+        f = pd.read_csv(dir+POP_FILE)
+        last = int(f.tail(1).values.tolist()[0][0])
+
+        if(last != GENRATIONS):
+          lost.append(seed)
+          print(dir + '===FOUND===NOGO')
+
+        else:
+          print(dir + '===FOUND===FINISHED')
+
+      else:
+        print(dir + '===NOGO')
+        lost.append(seed)
+
+
+  print('SEED INCOMPLETE/UNCREATED=', lost)
+  print('-----------------------------'*4)
+  print()
 
 
 def main():
   # Generate the arguments
   parser = argparse.ArgumentParser(description="Data aggregation script.")
   parser.add_argument("data_directory", type=str, help="Target experiment directory.")
+  parser.add_argument("selection", type=int, help="What selection do we want to check")
 
   # Get the arguments
   args = parser.parse_args()
   data_directory = args.data_directory
+  sel = args.selection
   print('Starting run_fin_cnt.py')
-  lex(data_directory)
+
+  # What are we checking
+  if(sel == 0):
+    lex(data_directory)
+
+  elif(sel == 1):
+    tour(data_directory)
 
 
 if __name__ == "__main__":
