460f116f81461dde170f66ce7a67d897160a3831
JoseHernandez9094 <josexavi06@gmail.com>
2019-12-05 17:18:09 -0500

    chaning for hpcc

--- Scrape/run_fin_cnt.py
+++ Scrape/run_fin_cnt.py
@@ -11,7 +11,6 @@ import datetime
 import argparse
 import os
 import pandas as pd
-import sys
 
 # VARIABLES EVERYONE NEEDS TO KNOW
 GENRATIONS=50000
@@ -22,7 +21,7 @@ REPLICATES = 100
 NOW = datetime.datetime.now()
 
 
-######################## LEXICASE STUFF ########################
+######################## LEXICASE ########################
 LEX_POP_SIZE = [10, 100, 500, 700, 1000]
 LEX_OFFSET = 0
 LEX_DIR_1 = "SEL_LEXICASE__DIA_Exploitation__POP_"
@@ -64,7 +63,7 @@ def lex(d_dir):
 
 
 
-######################## TOURNAMENT STUFF ########################
+######################## TOURNAMENT ########################
 TRN_SIZE = [7, 100, 500, 700, 1000]
 TRN_OFFSET = 500
 TRN_DIR_1 = "SEL_TOURNAMENT__DIA_Exploitation__POP___TRT_100__TOURN_"
@@ -105,6 +104,46 @@ def tour(d_dir):
   print()
 
 
+######################## DOWN SAMPLED ########################
+DSL_PROP = [.05, .1, .25, .5, 1.0]
+DSL_OFFSET = 1500
+DSL_DIR_1 = "SEL_DOWN__DIA_Exploitation__POP_1000__TRT_100__PROP_"
+DSL_DIR_2 = "__SEED_"
+
+def dsl(d_dir):
+  print('-----------------------------'*4)
+  print('Processing Down Sampled Runs-' + str(NOW))
+
+  lost = []
+
+  for i in range(len(DSL_PROP)):
+    for r in range(1,REPLICATES+1):
+      # Create the directory
+      seed = (r + (i * 100)) + DSL_OFFSET + REPLICATION_OFFSET
+      dir = d_dir + DSL_DIR_1 + str(DSL_PROP[i]) + DSL_DIR_2 + str(seed)
+
+      # Check to see if directory exists
+      if(os.path.isdir(dir)):
+        # Get the last row and check if we finished the run
+        f = pd.read_csv(dir+POP_FILE)
+        last = int(f.tail(1).values.tolist()[0][0])
+
+        if(last != GENRATIONS):
+          lost.append(seed)
+          print(dir + '===FOUND===NOGO')
+
+        else:
+          print(dir + '===FOUND===FINISHED')
+
+      else:
+        print(dir + '===NOGO')
+        lost.append(seed)
+
+
+  print('SEED INCOMPLETE/UNCREATED=', lost)
+  print('-----------------------------'*4)
+  print()
+
 def main():
   # Generate the arguments
   parser = argparse.ArgumentParser(description="Data aggregation script.")
@@ -123,6 +162,9 @@ def main():
 
   elif(sel == 1):
     tour(data_directory)
+
+  elif(sel == 2):
+    dsl(data_directory)
 
 
 if __name__ == "__main__":
