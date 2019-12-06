046c6ce2180bb357c8420e6afbcc11783c369826
JoseHernandez9094 <josexavi06@gmail.com>
2019-12-06 00:02:44 -0500

    added col  with pop size

--- Scrape/lex_pop_err.py
+++ Scrape/lex_pop_err.py
@@ -58,7 +58,7 @@ def lex(d_dir, w_dir, snap):
             # Check if data directory exists
             if(os.path.isdir(dir + str(seed))):
                 # Create data frame
-                data = pd.read_csv(dir + str(seed) + POP_FILE, index_col=False, header=['gen', 'avg_err', 'unique'])
+                data = pd.read_csv(dir + str(seed) + POP_FILE, index_col=False, header=[0,1,2])
 
                 # Grab every nth row
                 data = data.iloc[::snap, COL]
